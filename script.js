27d145d4aa76908a78e3e71250f9992827c697d9
Jacob Handelman <jacobhandelman@gmail.com>
2022-04-20 09:11:52 -0400

    added a module for the gameBoard which ties the board layout to the #ids on the dom

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-console.log("Hello World!")
 
 
 //Create a module for GameBoard
 const gameBoard = (() => {
-
   const boardLayout = ['top0', 'top1', 'top2',
-                      'middle0', 'middle1', 'middle2',
-                      'bottom0', 'bottom1', 'bottom2']
-
+                       'mid0', 'mid1', 'mid2',
+                       'bot0', 'bot1', 'bot2']
+  const tiles = document.querySelectorAll(
+  boardLayout.map(id => `#${id}`).join(', ')
+);
+  
+  console.log(tiles);
+  
   return{
     boardLayout
   };
 
 })();
+
+const moduleTest = (() => {
+ 
+ 
+})();
 console.log(gameBoard.boardLayout)
 //create a module for displayController
 
 //create a factory for players
+
+const createPlayer = () => {
+  let movesPlayed = 0;
+  return() => {
+    console.log(movesPlayed);
+    movesPlayed++;
+  }
+
+}
+
 
