6bcc7b5a295c95e99df20c8c1d5a0fd5b8501bcc
Jacob Handelman <jacobhandelman@gmail.com>
2022-04-20 17:40:47 -0400

    When X or O wins, gameInput gets disabled. Can now check if the outcome is a win or a tie, console.logs it with a function for now.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 //Create a module for GameBoard
 const gameBoard = (() => {
   const boardLayout = ['top0', 'top1', 'top2',
@@ -12,34 +11,116 @@ const gameBoard = (() => {
 })();
 //plays the game.
 const gameFlow = (() => {
-  const tileArray = gameBoard.tiles;
-  let player1Turn = true;
-  for (let i = 0; i < tileArray.length; i++) {
+  const _tileArray = gameBoard.tiles;
+  let _noInput = false;
+  let _winArrayX = []; // an array for moves of X
+  let _winArrayO = []; //an array for moves of O
+  let _player1Turn = true;
+  for (let i = 0; i < _tileArray.length; i++) {
     (function(index) {
-        tileArray[index].addEventListener("click", function() {
-        selectX = player1.playerMove();
-        selectO = player2.playerMove();
+        _tileArray[index].addEventListener("click", function() {
+        _selectX = player1.playerMove();
+        _selectO = player2.playerMove();
         let that = this; //id's of tiles
-          if (player1Turn === true && that.innerText === ''){
-          this.innerText = `${selectX}`;
-          player1Turn = false;
+          if (_player1Turn === true && that.innerText === '' && _noInput === false){
+          this.innerText = `${_selectX}`;
+          _winArrayX.push(that.id);
+          console.log(_winArrayX);
+          _player1Turn = false;
+          
+          
+          //create and array of where the X's are
+          //if array location = all of tops,bots,mids -- win
+          //all of 0, 1 or 2 -- win
+          //top0 mid1 bot2 || top2 mid1 bot0 -- win
+          
         }
-          else if(that.innerText === ''){
-          this.innerText = `${selectO}`;
-          player1Turn = true;
+          else if(that.innerText === '' && _noInput === false){
+          this.innerText = `${_selectO}`;
+          _winArrayO.push(that.id);
+          _player1Turn = true;
+          //create an array of where the O's are
           }
+          const gameStatus = (() => {        //put this in loop to check every time something is pressed
+            if(_winArrayX.includes('top0') &&_winArrayX.includes('top1') && _winArrayX.includes('top2')){
+              _gameOver('X')
+            }
+            else if(_winArrayX.includes('mid0') &&_winArrayX.includes('mid1') && _winArrayX.includes('mid2')){
+              _gameOver('X')
+            }
+            else if(_winArrayX.includes('bot0') &&_winArrayX.includes('bot1') && _winArrayX.includes('bot2')){
+              _gameOver('X')
+            }
+            else if(_winArrayX.includes('top0') &&_winArrayX.includes('mid0') && _winArrayX.includes('bot0')){
+              _gameOver('X')
+            }
+            else if(_winArrayX.includes('top1') &&_winArrayX.includes('mid1') && _winArrayX.includes('bot1')){
+              _gameOver('X')
+            }
+            else if(_winArrayX.includes('top2') &&_winArrayX.includes('mid2') && _winArrayX.includes('bot2')){
+              _gameOver('X')
+            }
+            else if(_winArrayX.includes('top0') &&_winArrayX.includes('mid1') && _winArrayX.includes('bot2')){
+              _gameOver('X')
+            }
+            else if(_winArrayX.includes('top2') &&_winArrayX.includes('mid1') && _winArrayX.includes('bot0')){
+              _gameOver('X')
+            }
+            
+            else if(_winArrayO.includes('top0') &&_winArrayO.includes('top1') && _winArrayO.includes('top2')){
+              _gameOver('O')
+            }
+            else if(_winArrayO.includes('mid0') &&_winArrayO.includes('mid1') && _winArrayO.includes('mid2')){
+              _gameOver('O')
+            }
+            else if(_winArrayO.includes('bot0') &&_winArrayO.includes('bot1') && _winArrayO.includes('bot2')){
+              _gameOver('O')
+            }
+            else if(_winArrayO.includes('top0') &&_winArrayO.includes('mid0') && _winArrayO.includes('bot0')){
+              _gameOver('O')
+            }
+            else if(_winArrayO.includes('top1') &&_winArrayO.includes('mid1') && _winArrayO.includes('bot1')){
+              _gameOver('O')
+            }
+            else if(_winArrayO.includes('top2') &&_winArrayO.includes('mid2') && _winArrayO.includes('bot2')){
+              _gameOver('O')
+            }
+            else if(_winArrayO.includes('top0') &&_winArrayO.includes('mid1') && _winArrayO.includes('bot2')){
+              _gameOver('O')
+            }
+            else if(_winArrayO.includes('top2') &&_winArrayO.includes('mid1') && _winArrayO.includes('bot0')){
+              _gameOver('O')
+            }
+            
+            else if (_winArrayO.length + _winArrayX.length === 9){
+              _gameOver('tie');
+            }
+            
+          })();
       })  
     })(i);
   } 
   const restartGame = (() => {
     const resetButton = document.querySelector("#reset");
     resetButton.addEventListener("click", function(){
-      for(i of tileArray){
+      for(i of _tileArray){
         i.innerText = '';
-        player1Turn = true; // X always starts
+        _player1Turn = true; // X always starts
+        _winArrayX = [];
+        _winArrayO = [];
+        _noInput = false;
       }
     });
   })();
+  const _gameOver = (winner) => { // the games ended because of a win or tie, disables and prompts
+    if (winner === 'tie'){
+      console.log("It's a tie!");
+      _noInput = true;
+    }
+   else{
+    _noInput = true;
+    console.log(`${winner} wins!`)
+  }}
 })();
 //create a factory for players
 const Player = (value) => {
