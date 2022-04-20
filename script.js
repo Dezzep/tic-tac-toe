beb351e1bc53dfac6b1459541031dd212b805b05
Jacob Handelman <jacobhandelman@gmail.com>
2022-04-19 20:00:46 -0400

    styled the tic tac toe interface on the dom

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Hello World!")
+
+
+//Create a module for GameBoard
+const gameBoard = (() => {
+
+  const boardLayout = ['top0', 'top1', 'top2',
+                      'middle0', 'middle1', 'middle2',
+                      'bottom0', 'bottom1', 'bottom2']
+
+  return{
+    boardLayout
+  };
+
+})();
+console.log(gameBoard.boardLayout)
+//create a module for displayController
+
+//create a factory for players
+
